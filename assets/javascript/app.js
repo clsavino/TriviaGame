5c900be2f574f0ac48a14d2b181eb653d04d4379
Christi Savino <c.savino@ymail.com>
2016-08-17 20:44:17 -0400

    working Trivia game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,74 @@
 window.onload = function(){
-    $('.gameBtn').on('click', trivia);
-};
 
-var counter = 20;
+var counter = 30;
 var timerId;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
+var myForm = '';
+var answers = ['howard', 'mystique', 'panther', 'asgard', 'laufey'];
+var answerChecked;
+var allRadios;
+var numQuestions = 5;
+
+$('.gameBtn').on('click', startTimer);
+
+function startTimer() {// runs once to start timer
+	//???????????????????????????????
+	//$('formDisplay').append(myForm);
+
+	// start the countdown here //
+	timerId = setInterval(displayTime, 1000);
+	$('.gameBtn').remove(); //removes Start button
+	triviaGame();
+}
+
+function displayTime() {
+	// increment counter by 1 - indicates # seconds since game began
+	counter--;
+	$('#infoDisplay').html('<p>Time Remaining: ' + counter + ' seconds</p>');
+	if (counter === 0) {
+		clearInterval(timerId);// clears timer
+		reset();
+	}
+}
+
+function triviaGame(){
+	$('form').removeClass('hide');
+}
 
 function reset() {
 	console.log('reset function');
 	counter = 20;
+	$('form').addClass('hide');
+	answerChecked = $("input[type=radio]:checked");
+	allRadios = $("input[type=radio]");
 
-	$('#formDisplay').html('<h2>All Done!</h2>');
-	$('#formDisplay').append('<h2>Correct Answers: ' + correct + '</h2>');
-	$('#formDisplay').append('<h2>Incorrect Answers: ' + incorrect + '</h2>');
-	$('#formDisplay').append('<h2>Unanswered: ' + unanswered + '</h2>');
-}
+	console.log(answerChecked[0].value, 'value');
 
-function trivia() {
-	// start the countdown here //
-	timerId = setInterval(display, 1000);
-	$('.gameBtn').remove();
-}
+	console.log($.inArray(answerChecked[1].value, answers));
+	//Determin correct and incorrect and unanswered questions
+	console.log('answerChecked ', answerChecked);
+	console.log('allRadios ', allRadios);
 
-function display() {
-	// increment time by 1
-	counter--;
-	console.log('counter is ' + counter);
-	$('#formDisplay').html('<h2>Time Remaining: ' + counter + ' seconds</h2>');
-	if (counter === 0) {
-		clearInterval(timerId);
-		reset();
+	// Check to see if the checked radio buttons are the correct answers. Compare to the answers array using the jQuery function -  $.inArray(value, array)
+	var i;
+	for (i=0; i < answerChecked.length; i++) {
+		var arrayIndex = $.inArray(answerChecked[i].value,answers);
+		if (arrayIndex >= 0) { //the checked btn was correct
+			correct++;
+			console.log(correct, 'correct');
+			console.log(arrayIndex, 'arrayIndex');
+		}
 	}
+	// calculate unanswered questions 
+	unanswered = numQuestions - answerChecked.length;
+	// calculate incorrect answers
+	incorrect = numQuestions - correct - unanswered;
+	$('#infoDisplay').append('<p>All Done!</p>');
+	$('#infoDisplay').append('<p>Correct Answers: ' + correct + '</p>');
+	$('#infoDisplay').append('<p>Incorrect Answers: ' + incorrect + '</p>');
+	$('#infoDisplay').append('<p>Unanswered: ' + unanswered + '</p>');
 }
+
+}; // end of window.onload
