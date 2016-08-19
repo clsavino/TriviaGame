fa255878456777b880c0bb2b45da341cffcc3680
Christi Savino <c.savino@ymail.com>
2016-08-19 19:15:43 -0400

    final changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,64 +1,58 @@
 window.onload = function(){
 
-var counter = 30;
+var counter = 40;
 var timerId;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
 var myForm = '';
-var answers = ['howard', 'mystique', 'panther', 'asgard', 'laufey'];
+var answers = ['howard', 'mystique', 'panther', 'asgard', 'natalia', 'bucky', 'gwen', 'laufey', 'magneto'];
+console.log('answers array ', answers);
 var answerChecked;
 var allRadios;
-var numQuestions = 5;
+var numQuestions = 9;
 
+// starts the game when game button (start) pressed
 $('.gameBtn').on('click', startTimer);
 
-function startTimer() {// runs once to start timer
-	//???????????????????????????????
-	//$('formDisplay').append(myForm);
-
+// runs once to start the timer when called by click handler
+function startTimer() {
 	// start the countdown here //
 	timerId = setInterval(displayTime, 1000);
-	$('.gameBtn').remove(); //removes Start button
+	$('.gameBtn').addClass('hide'); //removes Start button
+	$('#infoDisplay').html('<p></p>');
 	triviaGame();
 }
 
+// displays a timer of how many seconds left to play the game
 function displayTime() {
-	// increment counter by 1 - indicates # seconds since game began
+	// decrement counter by 1 - indicates # seconds left in game
 	counter--;
 	$('#infoDisplay').html('<p>Time Remaining: ' + counter + ' seconds</p>');
 	if (counter === 0) {
 		clearInterval(timerId);// clears timer
-		reset();
+		reset(); // removes game questions and displays stats
 	}
 }
 
+// displays the trivia game that was hidden until the start btn clicked
 function triviaGame(){
-	$('form').removeClass('hide');
+	$('form').removeClass('hide'); 
 }
 
+// resets the page by removing the trivia and displaying the stats
 function reset() {
-	console.log('reset function');
-	counter = 20;
+	counter = 40;
 	$('form').addClass('hide');
 	answerChecked = $("input[type=radio]:checked");
 	allRadios = $("input[type=radio]");
 
-	console.log(answerChecked[0].value, 'value');
-
-	console.log($.inArray(answerChecked[1].value, answers));
-	//Determin correct and incorrect and unanswered questions
-	console.log('answerChecked ', answerChecked);
-	console.log('allRadios ', allRadios);
-
 	// Check to see if the checked radio buttons are the correct answers. Compare to the answers array using the jQuery function -  $.inArray(value, array)
 	var i;
 	for (i=0; i < answerChecked.length; i++) {
-		var arrayIndex = $.inArray(answerChecked[i].value,answers);
-		if (arrayIndex >= 0) { //the checked btn was correct
+		var arrayIndex = $.inArray(answerChecked[i].value, answers);
+		if (arrayIndex >= 0) { //the checked btn was a correct answer (was in the answers array)
 			correct++;
-			console.log(correct, 'correct');
-			console.log(arrayIndex, 'arrayIndex');
 		}
 	}
 	// calculate unanswered questions 
@@ -69,6 +63,8 @@ function reset() {
 	$('#infoDisplay').append('<p>Correct Answers: ' + correct + '</p>');
 	$('#infoDisplay').append('<p>Incorrect Answers: ' + incorrect + '</p>');
 	$('#infoDisplay').append('<p>Unanswered: ' + unanswered + '</p>');
+
+	$('.gameBtn').removeClass('hide');
 }
 
 }; // end of window.onload
